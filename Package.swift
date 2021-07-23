b758473976d66ea6df061820d339b88bed1336f8
Jacob Rakidzich <jacobzivandesign@gmail.com>
2021-07-23 08:45:22 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/johnsundell/publish.git", from: "0.5.0"),
-        .package(url: "https://github.com/JZDesign/splash.git", from: "0.161.1")
+        .package(url: "https://github.com/JZDesign/splash.git", from: "0.161.2")
     ],
     targets: [
         .target(
